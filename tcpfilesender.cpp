745c15ab7db6894d1175f05b93ff3928d3e4a707
Sherry <41243205@nfu.edu.tw>
2024-11-18 17:46:44 +0800

    新增IP與PORT輸入

--- tcpfilesender.cpp
+++ tcpfilesender.cpp
@@ -7,12 +7,21 @@ TcpFileSender::TcpFileSender(QWidget *parent)
     totalBytes = 0;
     bytesWritten = 0;
     bytesToWrite = 0;
+
     clientProgressBar = new QProgressBar;
     clientStatusLabel = new QLabel(QStringLiteral("客戶端就緒"));
+
+    // 新增 IP 和 Port 輸入框
+    ipLabel = new QLabel(QStringLiteral("IP 位址："));
+    ipLineEdit = new QLineEdit("192.168.225.147");
+    portLabel = new QLabel(QStringLiteral("埠號："));
+    portLineEdit = new QLineEdit("16998");
+
     startButton = new QPushButton(QStringLiteral("開始"));
     quitButton = new QPushButton(QStringLiteral("退出"));
     openButton = new QPushButton(QStringLiteral("開檔"));
     startButton->setEnabled(false);
+
     buttonBox = new QDialogButtonBox;
     buttonBox->addButton(startButton, QDialogButtonBox::ActionRole);
     buttonBox->addButton(openButton, QDialogButtonBox::ActionRole);
@@ -21,65 +30,89 @@ TcpFileSender::TcpFileSender(QWidget *parent)
     QVBoxLayout *mainLayout = new QVBoxLayout;
     mainLayout->addWidget(clientProgressBar);
     mainLayout->addWidget(clientStatusLabel);
+
+    // 新增 IP 和 Port 到界面
+    QHBoxLayout *ipLayout = new QHBoxLayout;
+    ipLayout->addWidget(ipLabel);
+    ipLayout->addWidget(ipLineEdit);
+
+    QHBoxLayout *portLayout = new QHBoxLayout;
+    portLayout->addWidget(portLabel);
+    portLayout->addWidget(portLineEdit);
+
+    mainLayout->addLayout(ipLayout);
+    mainLayout->addLayout(portLayout);
+
     mainLayout->addStretch(1);
     mainLayout->addSpacing(10);
     mainLayout->addWidget(buttonBox);
+
     setLayout(mainLayout);
     setWindowTitle(QStringLiteral("(版本控制Git管理)檔案傳送"));
-    connect(openButton,SIGNAL(clicked()), this, SLOT(openFile()));
-    connect(startButton, SIGNAL(clicked()), this, SLOT(start()));
-    connect(&tcpClient, SIGNAL(connected()), this, SLOT(startTransfer()));
-    connect(&tcpClient, SIGNAL(bytesWritten(qint64)), this, SLOT(updateClientProgress(qint64)));
-    connect(quitButton, SIGNAL(clicked()), this, SLOT(close()));
 
+    connect(openButton, &QPushButton::clicked, this, &TcpFileSender::openFile);
+    connect(startButton, &QPushButton::clicked, this, &TcpFileSender::start);
+    connect(&tcpClient, &QTcpSocket::connected, this, &TcpFileSender::startTransfer);
+    connect(&tcpClient, &QTcpSocket::bytesWritten, this, &TcpFileSender::updateClientProgress);
+    connect(quitButton, &QPushButton::clicked, this, &TcpFileSender::close);
 }
+
 void TcpFileSender::openFile()
 {
     fileName = QFileDialog::getOpenFileName(this);
     if (!fileName.isEmpty()) startButton->setEnabled(true);
 }
+
 void TcpFileSender::start()
 {
     startButton->setEnabled(false);
     bytesWritten = 0;
+
+    // 讀取使用者輸入的 IP 和埠號
+    QString ipAddress = ipLineEdit->text();
+    quint16 port = portLineEdit->text().toUShort();
+
     clientStatusLabel->setText(QStringLiteral("連接中..."));
-    tcpClient.connectToHost(QHostAddress("127.0.0.1"), 16998);
+    tcpClient.connectToHost(QHostAddress(ipAddress), port);
 }
+
 void TcpFileSender::startTransfer()
 {
     localFile = new QFile(fileName);
     if (!localFile->open(QFile::ReadOnly))
-     {
-        QMessageBox::warning(this,QStringLiteral("應用程式"),
-                              QStringLiteral("無法讀取 %1:\n%2.").arg(fileName)
-                              .arg(localFile->errorString()));
+    {
+        QMessageBox::warning(this, QStringLiteral("應用程式"),
+                             QStringLiteral("無法讀取 %1:\n%2.").arg(fileName)
+                                 .arg(localFile->errorString()));
         return;
-     }
+    }
 
     totalBytes = localFile->size();
     QDataStream sendOut(&outBlock, QIODevice::WriteOnly);
     sendOut.setVersion(QDataStream::Qt_4_6);
     QString currentFile = fileName.right(fileName.size() -
-                                         fileName.lastIndexOf("/")-1);
-    sendOut <<qint64(0)<<qint64(0)<<currentFile;
+                                         fileName.lastIndexOf("/") - 1);
+    sendOut << qint64(0) << qint64(0) << currentFile;
     totalBytes += outBlock.size();
 
     sendOut.device()->seek(0);
-    sendOut<<totalBytes<<qint64((outBlock.size()-sizeof(qint64)*2));
+    sendOut << totalBytes << qint64((outBlock.size() - sizeof(qint64) * 2));
     bytesToWrite = totalBytes - tcpClient.write(outBlock);
     clientStatusLabel->setText(QStringLiteral("已連接"));
-    qDebug() << currentFile <<totalBytes;
+    qDebug() << currentFile << totalBytes;
     outBlock.resize(0);
 }
+
 void TcpFileSender::updateClientProgress(qint64 numBytes)
 {
     bytesWritten += (int) numBytes;
-    if(bytesToWrite > 0)
+    if (bytesToWrite > 0)
     {
         outBlock = localFile->read(qMin(bytesToWrite, loadSize));
-        bytesToWrite -= (int) tcpClient.write(outBlock);
+        bytesToWrite -= (int)tcpClient.write(outBlock);
         outBlock.resize(0);
-    }else
+    }
+    else
     {
         localFile->close();
     }
@@ -91,8 +124,4 @@ void TcpFileSender::updateClientProgress(qint64 numBytes)
 
 TcpFileSender::~TcpFileSender()
 {
-
 }
-
-
-
